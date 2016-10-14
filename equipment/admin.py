f4a5310abcb0ad8e17004d03c2450bfb74ebcc29
Kislov Dmitry <kislov@easydan.com>
2016-10-14 12:34:15 +1000

    Send mail to customer when the status of the app is changed

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,14 +1,27 @@
 #coding: utf-8
 from django.contrib import admin
-# from django.utils import timezone
-# from django.utils.translation import gettext as _
+from django.core.urlresolvers import reverse
+from django.core.mail import send_mail
 from .models import Equipment, Application
-
+from .messages import app_status, app_status_theme
 
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ('name', 'organization', 'startdate', 'enddate')
     list_filter = ('status', 'equipment', 'startdate', 'enddate', 'created')
     readonly_fields = ('unum',)
+
+    def save_model(self, *args): 
+        request, obj, form, change = args
+        if change:
+            if form.cleaned_data['status'] not in obj.mailsend:
+                send_mail(app_status_theme.format(obj.created),
+                          app_status.format(obj.name,
+                                            obj.created,
+                                            obj.get_status_display()
+                                            ),
+                          'equipment@botsad.ru', [obj.email,], fail_silently=True)
+        super(ApplicationAdmin, self).save_model(*args)
+
 
 class EquipmentAdmin(admin.ModelAdmin):
     pass
