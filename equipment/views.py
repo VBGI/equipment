ed86150d377f2bf383e1f12351b334683d062675
Kislov Dmitry <kislov@easydan.com>
2018-03-19 10:50:51 +1000

    unicode issue: fixed

--- equipment/views.py
+++ equipment/views.py
@@ -77,12 +77,12 @@ def request_rent(request):
                                                            enddate__gte=enddate,
                                                            equipment__pk=equip.pk).exclude(status='2').exists()
                 if start_intersect or end_intersect:
-                    extra = _('Обращаем Ваше внимание на то, что на данное'
-                              'время уже подана заявка. \n'
-                              'Уточните время у куратора уникальной научной установки'
-                              'или центра коллективного пользования.\n'
-                              'Контактные данные куратора приводятся на странице:\n'
-                              'http://botsad.ru/menu/aboutus/centry-kollektivnogo-polzovaniya')
+                    extra = _(u'Обращаем Ваше внимание на то, что на данное'
+                              u'время уже подана заявка. \n'
+                              u'Уточните время у куратора уникальной научной установки'
+                              u'или центра коллективного пользования.\n'
+                              u'Контактные данные куратора приводятся на странице:\n'
+                              u'http://botsad.ru/menu/aboutus/centry-kollektivnogo-polzovaniya')
 
                 else:
                     extra = ''
